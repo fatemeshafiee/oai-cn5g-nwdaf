140266cb7582695a5a8fc9162d70bea63e16c407
Abdelkader MEKRACHE <abdelkader.mekrache@eurecom.fr>
2023-10-17 20:54:44 +0200

    update readme and CLI

--- cli/nwdaf.py
+++ cli/nwdaf.py
@@ -27,29 +27,30 @@
 import json
 import requests
 import typer
-import logging
 from flask import Flask, request
 
+analytics_url = 'http://oai-nwdaf-nbi-gateway/nnwdaf-analyticsinfo/v1/analytics'
+subscription_url = 'http://oai-nwdaf-nbi-gateway/nnwdaf-eventssubscription/v1/subscriptions'
 app = typer.Typer()
-
 flask_app = Flask(__name__)
-flask_app.logger.setLevel(logging.ERROR)
 
+# Command for analytics
 @app.command()
 def analytics(
     json_file: str = typer.Argument(..., help="JSON analytics request file name"),
 ):
-    # Read JSON file
+    """
+    Perform analytics based on the provided JSON request file.
+    """
     with open(json_file) as file:
         data = json.load(file)
+
     # Extract parameters from JSON
     event_id = data['event-id']
     ana_req = json.dumps(data['ana-req'])
     event_filter = json.dumps(data['event-filter'])
     supported_features = json.dumps(data['supported-features'])
     tgt_ue = json.dumps(data['tgt-ue'])
-    # Construct the URL
-    analytics_url = 'http://oai-nwdaf-nbi-gateway/nnwdaf-analyticsinfo/v1/analytics'
     params = {
         'event-id': event_id,
         'ana-req': ana_req,
@@ -57,35 +58,35 @@ def analytics(
         'supported-features': supported_features,
         'tgt-ue': tgt_ue
     }
-    # Send GET request
     response = requests.get(analytics_url, params=params)
-    # Parse the JSON response
     parsed_response = json.loads(response.text)
-    # Print the result as formatted JSON
     typer.echo(json.dumps(parsed_response, indent=4))
-    
+
+# Command for subscribing to events
 @app.command()
 def subscribe(
     subscription_file: str = typer.Argument(..., help="JSON subscription file name"),
 ):
-    # Define the URL to send the subscription request to
-    subscription_url = 'http://oai-nwdaf-nbi-gateway/nnwdaf-eventssubscription/v1/subscriptions'
-    # Load subscription data from the JSON file
+    """
+    Subscribe to events based on the provided JSON subscription file.
+    """
     with open(subscription_file) as f:
         subscription_data = json.load(f)
-    # Send the subscription request
     response = requests.post(subscription_url, json=subscription_data)
+    # Handle response if necessary
 
     @flask_app.route('/notification', methods=['POST'])
     def handle_notification():
+        """
+        Endpoint to handle incoming notifications.
+        """
         data = request.json
         notif_data = data
         typer.echo(f'\nReceived notification for event: {notif_data["event"]}')
-        # Print notif_data in JSON format
         typer.echo(json.dumps(notif_data, indent=4))
         return 'OK'
-      
-    flask_app.run(host='0.0.0.0', port=3000, debug=True)    
-    
+
+    flask_app.run(host='0.0.0.0', port=3000, debug=True)
+
 if __name__ == '__main__':
     app()
