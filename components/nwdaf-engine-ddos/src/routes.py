816015e0b62007626a8fe1961bc781ce912c547e
Fatemeh Shafiei <ardestanifateme0@gmail.com>
2024-05-28 22:38:58 -0400

    the Demo version

--- components/nwdaf-engine-ddos/src/routes.py
+++ components/nwdaf-engine-ddos/src/routes.py
@@ -39,50 +39,58 @@ logging.basicConfig(level=logging.INFO)
 @api.route('/abnormal_behaviour/ddos', methods=['GET'])
 def handle_ddos_detection_request():
     df = create_dataframe()
+    ddos_entries = []
     logging.info(df[["Dst IP", "Dst Port", "Src IP", "Src Port", "Protocol", "pduseid", "seid"]])
     # Get the usefull rows of the DataFrame.
+    # These are the features we care about
+    # ['Flow Duration', 'Fwd Packet Length Std', 'ACK Flag Count', 'Protocol', 'Total Fwd Packet', 'Total Bwd packets',
+    # 'Total Length of Fwd Packet', 'Total Length of Bwd Packet']
     X = df.iloc[:, :-2]
-    scalar = StandardScaler(copy=True, with_mean=True, with_std=True)
-    scalar.fit(X)
-    X = scalar.transform(X)
+    for ip in X ["Src IP"].unique():
+        X_PER_UE  = X[X["Src IP"]==ip]
+        df_per_ue = df[df["Src IP"]==ip]
+        scalar = StandardScaler(copy=True, with_mean=True, with_std=True)
+        scalar.fit(X_PER_UE)
+        X_PER_UE = scalar.transform(X_PER_UE)
 
-    features = len(X[0])
-    samples = X.shape[0]
-    logging.info(X.shape)
+        features = len(X_PER_UE[0])
+        samples = X_PER_UE.shape[0]
+        logging.info(X_PER_UE.shape)
 
-    train_len = 5 #would it make any problem? When we have less packets?
-    input_len = samples - train_len
-    I = np.zeros((samples - train_len, train_len, features))
+        train_len = 5 #would it make any problem? When we have less packets?
+        input_len = samples - train_len
+        I = np.zeros((samples - train_len, train_len, features))
 
-    for i in range(input_len):
-        temp = np.zeros((train_len, features))
-        for j in range(i, i + train_len - 1):
-            temp[j-i] = X[j]
-        I[i] = temp
-    predict = DDoS_Detection_model.predict(I)
+        for i in range(input_len):
+            temp = np.zeros((train_len, features))
+            for j in range(i, i + train_len - 1):
+                temp[j-i] = X_PER_UE[j]
+            I[i] = temp
+        predict = DDoS_Detection_model.predict(I)
 
-    predictn = predict.flatten().round()
-    df = df[train_len:len(X)]
+        predictn = predict.flatten().round()
+        
+        df_per_ue = df_per_ue[train_len:len(X_PER_UE)]
 #     suspicious_Flows = df[predictn == 0]
-    predictn = predictn.tolist()
+        predictn = predictn.tolist()
 
 
-    suspicious_ues = df["Src IP"].tolist()
-    targets = df["Dst IP"].tolist()
-    suspicious_pdu_seid = df["pduseid"].tolist()
-    suspicious_seid = df["seid"].tolist()
-    ratios = predict.flatten().tolist()
-    ddos_entries = []
-    for ue_ip, target, pdu_seid, seid, ratio in zip(suspicious_ues, targets, suspicious_pdu_seid, suspicious_seid, ratios):
-        if not np.isnan(ratio) and ratio > 0.5:
-            ddos_entries.append({
+        suspicious_ues = df_per_ue["Src IP"].tolist()
+        targets = df_per_ue["Dst IP"].tolist()
+        suspicious_pdu_seid = df_per_ue["pduseid"].tolist()
+        suspicious_seid = df_per_ue["seid"].tolist()
+        ratios = predict.flatten().tolist()
+    
+        for ue_ip, target, pdu_seid, seid, ratio in zip(suspicious_ues, targets, suspicious_pdu_seid, suspicious_seid, ratios):
+            if not np.isnan(ratio) and ratio > 0.5:
+                ddos_entries.append({
                 "ue_ip":".".join(str(ipaddress.ip_address(ue_ip)).split(".")[::-1]),
                 "target_ip":".".join(str(ipaddress.ip_address(target)).split(".")[::-1]),
                 "pdu_sess_id":pdu_seid,
                 "seid":seid,
                 "ratio": ratio
 
-            })
+                })
     
 
     response_data = {'ddos_entries': ddos_entries}
