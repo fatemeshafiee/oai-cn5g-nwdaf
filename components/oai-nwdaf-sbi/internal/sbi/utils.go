e7d38b0eb874014a0190e8d8f998665d2bad5959
Fatemeh Shafiei <ardestanifateme0@gmail.com>
2025-11-01 20:26:30 -0400

    VOLUME_MEASUREMENT, PER_FLOW, RepPeriod=3

--- components/oai-nwdaf-sbi/internal/sbi/utils.go
+++ components/oai-nwdaf-sbi/internal/sbi/utils.go
@@ -212,7 +212,7 @@ func upfEventEventSubscription(upfEventNotifyUri string,
 	granType := []upf_client.GranularityOfMeasurement{upf_client.PER_FLOW}
 	// change the  time of UPF event exposure subscription here.
 	//func NewNupfEventExposure(NfId string, EventNotifyUri string, Triger UpfEventTrigger, NotifyCorrelationId string, RequestedEventTypes []EventType, RepPeriod int, measurementType [][]MeasurementType, measurement []GranularityOfMeasurement, anyUE bool)
-	var RepPeriod = 1
+	var RepPeriod = 3
 	subs := upf_client.NewNupfEventExposure(upfNfId, upfEventNotifyUri, upf_client.PERIODIC, upfNotifyCorrelationId, req_type, RepPeriod, measurementType, granType, true)
 	res, err := cli.CreateSubscription(subs)
 	if err != nil {
